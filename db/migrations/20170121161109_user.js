91d39be82abfcefd18bead3d1684d76863f4c154
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 16:36:50 -0700

    finished users migration

--- db/migrations/20170121161109_user.js
+++ db/migrations/20170121161109_user.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(users) {
+    users.increments('id');
+    users.string('display_name', 255).notNullable();
+    users.text('bio');
+    users.string('email', 255).notNullable();
+    users.string('password', 255).notNullable();
+    users.string('location', 255);
+    users.enu('remote_irl', ['remote', 'irl', 'both']).defaultTo('both').notNullable();
+    users.string('phone', 255);
+    users.boolean('email_notifications');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
