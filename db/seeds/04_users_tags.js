5e91b775257d9a3fec761b9149a0c357cfd297e8
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-01-22 00:55:43 -0700

    Added records to users_tags seed

--- db/seeds/04_users_tags.js
+++ db/seeds/04_users_tags.js
@@ -41,14 +41,70 @@ exports.seed = function(knex, Promise) {
                 }),
                 knex('users_tags').insert({
                     id: 6,
-                    user: 3,
+                    user: 5,
                     tag: 2,
                     interest: 'learn',
                     skill_level: 'intermediate'
                 }),
                 knex('users_tags').insert({
                     id: 7,
-                    user: 1,
+                    user: 6,
+                    tag: 2,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 8,
+                    user: 7,
+                    tag: 1,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 9,
+                    user: 7,
+                    tag: 5,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 10,
+                    user: 4,
+                    tag: 7,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 11,
+                    user: 4,
+                    tag: 6,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 12,
+                    user: 4,
+                    tag: 5,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 13,
+                    user: 3,
+                    tag: 2,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 14,
+                    user: 3,
+                    tag: 1,
+                    interest: 'learn',
+                    skill_level: 'intermediate'
+                }),
+                knex('users_tags').insert({
+                    id: 15,
+                    user: 3,
                     tag: 6,
                     interest: 'learn',
                     skill_level: 'intermediate'
