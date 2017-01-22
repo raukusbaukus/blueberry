c0e3a03233f75f69cc2967cd0ac4c0c78b0b34b5
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 17:29:39 -0700

    Migrated Tags table

---
+++ db/migrations/20170121170451_tags.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('tags', function(tags) {
+    tags.increments('id');
+    tags.string('title', 255);
+    tags.integer('popularity').notNullable().defaultTo(0);
+    tags.integer('user').references('users.id');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('tags');
+};
