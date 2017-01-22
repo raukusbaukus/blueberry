3782ef1ed23af1862d4a083f527b860e10fd5fe5
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 18:51:08 -0700

    Migrate and Seeded Users_Tags

--- db/migrations/20170121182046_users_tags.js
+++ db/migrations/20170121182046_users_tags.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users_tags', function(users_tags) {
+    users_tags.increments('id');
+    users_tags.integer('user').references('users.id').notNullable();
+    users_tags.integer('tag').references('tags.id').notNullable();
+    users_tags.enu('interest', ['teach', 'learn']).notnullable();
+    users_tags.enu('skill_level', ['beginner', 'intermediate', 'advanced']);
+    users_tags.unique(['user', 'tag', 'interest']);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users_tags');
 };
--- db/seeds/04_users_tags.js
+++ db/seeds/04_users_tags.js
@@ -1,13 +1,52 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users_tags').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('users_tags').insert({id: 1,
+          user: 1,
+          tag: 2,
+          interest: 'learn',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 2,
+          user: 2,
+          tag: 2,
+          interest: 'learn',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 2,
+          user: 1,
+          tag: 2,
+          interest: 'teach',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 2,
+          user: 2,
+          tag: 2,
+          interest: 'teach',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 2,
+          user: 4,
+          tag: 2,
+          interest: 'learn',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 2,
+          user: 3,
+          tag: 2,
+          interest: 'learn',
+          skill_level: 'intermediate'
+        }),
+        knex('users_tags').insert({id: 3,
+          user: 1,
+          tag: 6,
+          interest: 'learn',
+          skill_level: 'intermediate'
+        })
       ]);
     });
 };
