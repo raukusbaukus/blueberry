73d6b7011b4e53b7e158dca9c229af07bb842933
Benjamin Matson <benjaminmatson@gmail.com>
2017-01-22 12:49:37 -0700

    basic express route setup

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const func = require('../src/app.js'); //your file needs to export the functions you want to test
+const assert = require('assert');
+
+describe('function name', function() {
+  // describe('#functionName', function() { //replace with your function name
+  //   it('this is the expected result of the above function call', function() {
+  //     var actual = app.functionName('abcdefg'); //replace this with real names and params
+  //     var expected = 'this is the expected result of the above function call';
+  //     assert.equal(actual, expected, "This function should do X.");
+  //   });
+  //   it('this is the expected result of the above function call', function() {
+  //     var actual = func.reverseStr(12345);
+  //     var expected = '';
+  //     assert.equal(actual, expected, "Reversing the number 12345 should give an empty string.");
+  //   });
+  // });
+});
