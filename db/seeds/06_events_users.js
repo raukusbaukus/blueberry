b5842f20f6cbef4ba50ca043dee9b719fa8643e1
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 19:10:11 -0700

    Seeded Events_Users

--- db/seeds/06_events_users.js
+++ db/seeds/06_events_users.js
@@ -1,13 +1,54 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('events_users').del()
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
+        knex('events_users').insert({
+          id: 1,
+          'event': 1,
+          user: 2,
+          role: 'student',
+          rating: 3,
+          review: 'Meh. It was ok.'
+        }),
+        knex('events_users').insert({
+          id: 2,
+          'event': 3,
+          user: 2,
+          role: 'student'
+        }),
+        knex('events_users').insert({
+          id: 2,
+          'event': 1,
+          user: 4,
+          role: 'student'
+        }),
+        knex('events_users').insert({
+          id: 2,
+          'event': 3,
+          user: 4,
+          role: 'teacher'
+        }),
+        knex('events_users').insert({
+          id: 2,
+          'event': 1,
+          user: 1,
+          role: 'student'
+        }),
+        knex('events_users').insert({
+          id: 2,
+          'event': 3,
+          user: 1,
+          role: 'student'
+        }),
+        knex('events_users').insert({
+          id: 3,
+          'event': 4,
+          user: 1,
+          role: 'student'
+        })
       ]);
     });
 };
