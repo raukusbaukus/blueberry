ccd1aa254508dacdc7abc2be4bbf0445b27b60a8
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 18:57:13 -0700

    Seeded and Migrated Events_Tags

--- db/migrations/20170121182115_events_tags.js
+++ db/migrations/20170121182115_events_tags.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex, Promise) {
-  
+    return knex.schema.createTable('events_tags', function(events_tags) {
+        events_tags.integer('event').references('events.id').notNullable();
+        events_tags.integer('tag').references('tags.id').notNullable();
+        events_tags.primary(['event', 'tag']);
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema.dropTable('events_tags');
 };
--- db/seeds/05_events_tags.js
+++ db/seeds/05_events_tags.js
@@ -1,13 +1,41 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('events_tags').del()
+        .then(function() {
+            return Promise.all([
+                // Inserts seed entries
+                knex('events_tags').insert({
+                    event: 2,
+                    tag: 2;
+                }),
+                knex('events_tags').insert({
+                    event: 2,
+                    tag: 3;
+                }),
+                knex('events_tags').insert({
+                    event: 2,
+                    tag: 4;
+                }),
+                knex('events_tags').insert({
+                    event: 3,
+                    tag: 3;
+                }),
+                knex('events_tags').insert({
+                    event: 3,
+                    tag: 1;
+                }),
+                knex('events_tags').insert({
+                    event: 3,
+                    tag: 5;
+                }),
+                knex('events_tags').insert({
+                    event: 1,
+                    tag: 3;
+                }),
+                knex('events_tags').insert({
+                    event: 3,
+                    tag: 6;
+                })
+            ]);
+        });
 };
