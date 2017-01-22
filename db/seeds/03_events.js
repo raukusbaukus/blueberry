0d094f602bd453fddb30758d6746aae4a5676c6b
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-21 18:41:05 -0700

    Seeded Events

--- db/seeds/03_events.js
+++ db/seeds/03_events.js
@@ -1,13 +1,39 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+    // Deletes ALL existing entries
+    return knex('events').del()
+        .then(function() {
+            return Promise.all([
+                // Inserts seed entries
+                knex('events').insert({
+                    id: 1,
+                    title: 'Node',
+                    user: 1,
+                    start: '2017-01-28 1200:00',
+                    end: '2017-01-28 13:00:00',
+                    description: 'The awesomest event ever',
+                    skill_level: 'beginner',
+                    capacity: 50
+                }),
+                knex('events').insert({
+                    id: 2,
+                    title: 'JavaScript',
+                    user: 1,
+                    start: '2017-01-28 12:00:00',
+                    end: '2017-01-28 13:00:00',
+                    description: 'The awesomest event ever',
+                    skill_level: 'beginner',
+                    capacity: 50
+                }),
+                knex('events').insert({
+                    id: 3,
+                    title: 'NPM',
+                    user: 1,
+                    start: '2017-01-28 12:00:00',
+                    end: '2017-01-28 13:00:00',
+                    description: 'The awesomest event ever',
+                    skill_level: 'beginner',
+                    capacity: 50
+                })
+            ]);
+        });
 };
