ed31909c3061801f4034b7523639ad90a10fcd68
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-01-22 00:51:24 -0700

    Added Records to Event Tags Seed

--- db/seeds/05_events_tags.js
+++ db/seeds/05_events_tags.js
@@ -5,35 +5,63 @@ exports.seed = function(knex, Promise) {
             return Promise.all([
                 // Inserts seed entries
                 knex('events_tags').insert({
-                    event: 2,
-                    tag: 2
-                }),
-                knex('events_tags').insert({
-                    event: 2,
-                    tag: 3
-                }),
-                knex('events_tags').insert({
-                    event: 2,
-                    tag: 4
-                }),
-                knex('events_tags').insert({
-                    event: 3,
-                    tag: 3
-                }),
-                knex('events_tags').insert({
-                    event: 3,
+                    event: 1,
                     tag: 1
                 }),
                 knex('events_tags').insert({
-                    event: 3,
-                    tag: 5
+                    event: 1,
+                    tag: 2
                 }),
                 knex('events_tags').insert({
                     event: 1,
                     tag: 3
                 }),
                 knex('events_tags').insert({
+                    event: 1,
+                    tag: 4
+                }),
+                knex('events_tags').insert({
+                    event: 1,
+                    tag: 5
+                }),
+                knex('events_tags').insert({
+                    event: 1,
+                    tag: 6
+                }),
+                knex('events_tags').insert({
+                    event: 1,
+                    tag: 7
+                }),
+                knex('events_tags').insert({
+                    event: 2,
+                    tag: 7
+                }),
+                knex('events_tags').insert({
                     event: 3,
+                    tag: 6
+                }),
+                knex('events_tags').insert({
+                    event: 4,
+                    tag: 5
+                }),
+                knex('events_tags').insert({
+                    event: 4,
+                    tag: 4
+                }),
+                knex('events_tags').insert({
+                    event: 5,
+                    tag: 3
+                }),
+                knex('events_tags').insert({
+                    event: 5,
+                    tag: 2
+                }),
+                knex('events_tags').insert({
+                    event: 6,
+                    tag: 3
+                }),
+                knex('events_tags').insert({
+                    event: 7,
                     tag: 6
                 })
             ]);
