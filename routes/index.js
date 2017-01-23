492939e643d97aa2740386969dc44d73c4e85e32
Benjamin Matson <benjaminmatson@gmail.com>
2017-01-23 10:40:38 -0700

    added promise structure for tags query

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,50 @@
 const express = require('express'),
-    router = express.Router(),
-    queries = require('../queries');
+  router = express.Router(),
+  queries = require('../queries');
 
 router.get('/', (req, res, next) => {
-    queries.get_events()
-        .then(values => {
-            let events = [];
-            values.forEach(value => {
-                let event = {
-                    title: value.title,
-                    location: {
-                        title: value.venue,
-                        address: value.address,
-                        area: value.area
-                    },
-                    start: value.start,
-                    end: value.end,
-                    tags: [],
-                    host: {
-                        avatar: value.avatar,
-                        display_name: value.display_name,
-                        bio: value.bio,
-                        rating: value.rating,
-                        xp: value.xp,
-                    },
-                    description: value.description,
-                    skill_level: value.skill_level,
-                    capacity: value.capacity
-                }
-                events.push(event);
-            });
-            res.render('index', {'events': events});
-        }).catch(err => {
-            console.error(err)
-            res.status(400).send('hey!' + err);
-        })
+  queries.get_tags()
+    .then(values => {
+
+    }).then(
+      queries.get_events()
+      .then(values => {
+        let events = [];
+        values.forEach(value => {
+          let event = {
+            title: value.title,
+            location: {
+              title: value.venue,
+              address: value.address,
+              area: value.area
+            },
+            start: value.start,
+            end: value.end,
+            tags: [],
+            host: {
+              avatar: value.avatar,
+              display_name: value.display_name,
+              bio: value.bio,
+              rating: value.rating,
+              xp: value.xp,
+            },
+            description: value.description,
+            skill_level: value.skill_level,
+            capacity: value.capacity
+          }
+          events.push(event);
+        });
+        res.render('index', {
+          'events': events
+        });
+      }).catch(err => {
+        console.error(err)
+        res.status(400).send('hey!' + err);
+      })
+    ).catch(err => {
+      console.error(err)
+      res.status(400).send('hey!' + err);
+    });
 });
 
 module.exports = router;
