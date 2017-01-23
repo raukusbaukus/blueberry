aa87a7aa98846391244e825c7308bbcd3a8726ec
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-23 13:50:48 -0700

    Corrected Object Example for Date and Venue

--- routes/index.js
+++ routes/index.js
@@ -1,61 +1,42 @@
 const express = require('express'),
-  router = express.Router(),
-  queries = require('../queries');
+    router = express.Router(),
+    queries = require('../queries');
 
 router.get('/', (req, res, next) => {
-  queries.get_events()
-    .then(values => {
-      let events = [];
-      queries.get_tags()
-        .then(tag_values => {
-          let tags = [];
-          tag_values.forEach(tag_value => {
-            let tag = {
-              title: tag_value.title,
-              event: tag_value.event
-            }
-            tags.push(tag);
-          });
-          values.forEach(value => {
-            let event = {
-              id: value.id,
-              title: value.title,
-              location: {
-                title: value.venue,
-                address: value.address,
-                area: value.area
-              },
-              start: value.start,
-              end: value.end,
-              tags: [],
-              host: {
-                avatar: value.avatar,
-                display_name: value.display_name,
-                bio: value.bio,
-                rating: value.rating,
-                xp: value.xp,
-              },
-              description: value.description,
-              skill_level: value.skill_level,
-              capacity: value.capacity
-            }
-            let filtered_tags = tags.filter(tag => tag.event === event.id);
-            filtered_tags.forEach(tag => event.tags.push(tag.title));
-            events.push(event);
-          });
-          res.render('index', {
-            events: events
-          });
+    queries.get_events()
+        .then(values => {
+            let events = [];
+            values.forEach(value => {
+                let event = {
+                    id: value.id,
+                    title: value.title,
+                    location: {
+                        title: value.venue,
+                        address: value.address,
+                        area: value.area
+                    },
+                    start: value.start,
+                    end: value.end,
+                    tags: [],
+                    host: {
+                        avatar: value.avatar,
+                        display_name: value.display_name,
+                        bio: value.bio,
+                        rating: value.rating,
+                        xp: value.xp,
+                    },
+                    description: value.description,
+                    skill_level: value.skill_level,
+                    capacity: value.capacity
+                }
+                events.push(event);
+            });
+            console.log(events);
+            res.render('index', {events: events});
+        }).catch(err => {
+            console.error(err)
+            res.status(400).send('hey!' + err);
         })
-        .catch(err => {
-          console.error(err)
-          res.status(500).send(err);
-        })
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(500).send(err);
-    });
 });
 
 module.exports = router;
