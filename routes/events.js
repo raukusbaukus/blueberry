f373d6c1fbca695131c85aac4b1ced8fa9a161f2
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-01-23 17:26:10 -0700

    Refactored some EJS. Adding Event Search Route

---
+++ routes/events.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+  router = express.Router(),
+  query = require('../queries');
+
+  router.get('./events', (req, res) => {
+
+  });
+
+  module.exports = router;
