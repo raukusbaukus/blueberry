6ebe549e30640d0529d5583b97946151122e8264
Benjamin Matson <benjaminmatson@gmail.com>
2017-01-25 09:00:11 -0700

    pushing to tag_search branch

--- app.js
+++ app.js
@@ -9,22 +9,5 @@ app.use("/", routes);
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 3000);
-// const arg = process.argv[2] ? Number(process.argv[2]) : 3000;
-//
-// app.listen(arg, function() {
-//     console.log('App is listening on port ' + arg);
-// });
 
-
-// app.get('/landing', (req, res) => {
-//     // var html = new EJS({url: 'index.ejs'}).render();
-//     res.render('../views/index.ejs');
-//     // knex.select('*').from('events').then((payload) => {
-//     //   if (payload.length > 0) {
-//     //     res.status(200).send(payload); //JSON.stringify(payload));
-//     //   } else {
-//     //     res.status(404).send('No events found.');
-//     //   }
-//     // });
-// });
 module.exports = app;
