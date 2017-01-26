4a21a0b07d4c4733c3c5ac621024776b961c0fe9
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-01-25 19:17:27 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express'),
     hbs = require('hbs'),
     path = require('path'),
     routes = require('./routes/index'),
+    login = require('./routes/login'),
     passport = require('passport'),
     local_strategy = require('passport-local').Strategy,
     session = require('express-session'),
@@ -12,37 +13,11 @@ const express = require('express'),
 
 hbs.localsAsTemplateData(app);
 app.use(body_parser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-
-passport.use(new local_strategy({
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-        // session: false
-    },
-
-    (req, email, password, done) => {
-        query.find_user(email).then(user => {
-                console.log('found a user', user)
-            })
-            .catch(err => {
-                console.error('error', err);
-            })
-        // if (!user) { return done(null, false); }
-        // if (!user.verifyPassword(password)) { return done(null, false); }
-        // return done(null, user);
-    }));
-
-
-app.post('/login', passport.authenticate('local'), (req, res) => {
-    console.log('HI!', req);
-    res.redirect('/users/' + req.user.username);
-});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", routes);
+app.use('/login', login);
 // set the view engine to hbs
 app.set('view engine', 'hbs');
 app.set('port', process.env.PORT || 3000);
