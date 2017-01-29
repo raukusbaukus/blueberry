cf764dc9ba4ed0cb10c7844a5d1ce3f13d33e738
Benjamin Matson <benjaminmatson@gmail.com>
2017-01-29 13:12:41 -0700

    working on tags routes

--- public/js/tag_tool.js
+++ public/js/tag_tool.js
@@ -4,20 +4,39 @@ $(document).ready(function() {
     event.preventDefault();
     let tag = $('#tag-search-box').val();
     let user_id = $('#user_id').val();
-    console.log(tag + ' ' + user_id);
     $.ajax({
       type: "POST",
       url: "/tags/create",
       data: {tag, user_id},
-      success: display_tags,
+      success: prep_tags,
       dataType: "json"
     });
   });
-
-  function display_tags() {
-    console.log('displaying tags');
-  }
 });
 
+function prep_tags() {
+  console.log('prepping tags');
+  let user_id = $('#user_id').val();
+  $.ajax({
+    type: "GET",
+    url: `/user/${user_id}`,
+    success: show_tags,
+    dataType: "json"
+  })
+}
 
-//method="POST" action="/events/check_tag/"
+function show_tags() {
+  console.log("showing tags");
+}
+
+function remove_tag_from_user() {
+  let tag_name = '';
+  let user_id = $('#user_id').val();
+  $.ajax({
+    type: "DELETE",
+    url: "/tags/delete",
+    data: {tag_name, user_id},
+    success: prep_tags,
+    dataType: "json"
+  });
+}
