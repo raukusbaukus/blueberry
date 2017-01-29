a3459c5dbd4b81d712f26d036b8871cd3eb148f5
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-01-29 12:57:21 -0700

    Initial Commit

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,50 @@
 const express = require('express'),
     router = express.Router(),
+    argon2 = require('argon2'),
     query = require('../queries');
 //
 router.get('/create/', (req, res) => {
-  res.render('create_user');
+    res.render('create_user');
 });
 router.get('/create/:event', (req, res) => {
     let event = Number(req.params.event);
-        //if user arrives at signup via event rsvp, save event id to sign them up
-        //automagically, post registration
-        res.render('create_user', {
-            event
-        })
+    //if user arrives at signup via event rsvp, save event id to sign them up
+    //automagically, post registration
+    res.render('create_user', {
+        event
+    })
 });
 router.post('/create', (req, res) => {
-    let data = req.body;
-    query.create_user(data)
-        .then(me => {
-          //previous promise returns an array
-          me = me[0];
-            res.render('user', {
-                me
-            });
+    argon2.generateSalt(16).then(salt => {
+            argon2.hash(req.body.password, salt)
+                .then(hash => {
+                    req.body.password = hash;
+
+                    query.create_user(req.body)
+                        .then(me => {
+                            //previous promise returns an array
+                            me = me[0];
+                            res.render('user', {
+                                me
+                            });
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.status(400).send(err)
+                        })
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.render('error', {
+                        err
+                    })
+                })
         })
         .catch(err => {
             console.error(err);
-            res.status(400).send(err)
+            res.render('error', {
+                err
+            })
         })
 });
 router.get('/read/:id', (req, res) => {
