6b4765d749bbea8c520d30b134f56d79c7a620b0
Patrick Rauls <patrickrauls@Patricks-Air.domain>
2017-01-30 02:35:43 -0700

    initial commit

--- routes/event.js
+++ routes/event.js
@@ -1,166 +1,226 @@
 const express = require('express'),
-  router = express.Router(),
-  query = require('../queries');
+    router = express.Router(),
+    query = require('../queries');
 
 //CREATE
-router.get('/create/:user', (req, res) => {
-  query.get_all_tags()
-    .then(tags => {
-      let me = {
-        id: req.params.user
-      }
-      res.render('create_event', {
-        me,
-        tags
-      })
-    })
-    .catch(err => {
-      console.error('get all tags', err);
-      res.status(400).send(err);
-    })
+router.get('/create', (req, res) => {
+    if (req.session.user) {
+        query.get_all_tags()
+            .then(tags => {
+                let me = {
+                    id: req.session.user
+                }
+                res.render('create_event', {
+                    me,
+                    tags
+                })
+            })
+            .catch(err => {
+                console.error('get all tags', err);
+                res.render('error', {
+                    err
+                });
+            })
+    } else {
+        req.session.cookie.path = '/event/create';
+        res.redirect('/login?e=restricted');
+    }
 })
 
 router.post('/create', (req, res, next) => { //take path from moh's form
-  query.create_event(req.body)
-    .then(event => {
-      query.add_tags_to_event(req.body.tags, event)
-        .then(event => {
-          res.redirect('/');
-        })
-        .catch(err => {
-          console.error('add tags to event', err);
-          res.status(400).send(err)
-        })
-    })
-    .catch(err => {
-      console.error('create event', err);
-      res.status(400).send(err)
-    })
+    if (req.session.user) {
+        query.create_event(req.body)
+            .then(event => {
+                query.add_tags_to_event(req.body.tags, event)
+                    .then(event => {
+                        res.redirect('/');
+                    })
+                    .catch(err => {
+                        console.error('add tags to event', err);
+                        res.render('error', {
+                            err
+                        })
+                    })
+            })
+            .catch(err => {
+                console.error('create event', err);
+                res.render('error', {
+                    err
+                })
+            })
+    } else {
+        res.redirect('/login?e=restricted');
+    }
 });
 
 //READ
 router.get('/read/:id', (req, res) => {
-  let id = req.params.id;
-  query.get_event_by_id(id)
-    .then(db_event => {
-      let event = {
-        title: db_event[0].title,
-        description: db_event[0].description,
-        skill_level: db_event[0].skill_level,
-        capacity: db_event[0].capacity,
-        location: {
-          title: db_event[0].venue,
-          address: db_event[0].address,
-          area: db_event[0].area
-        },
-        date: db_event[0].start.toLocaleDateString('en-us', {
-          weekday: 'long',
-          month: 'long',
-          day: 'numeric',
-          year: 'numeric'
-        }),
-        start: db_event[0].start.toLocaleTimeString('en-us', {
-          hour: 'numeric',
-          minute: '2-digit'
-        }),
-        end: db_event[0].end.toLocaleTimeString('en-us', {
-          hour: 'numeric',
-          minute: '2-digit'
-        }),
-        host: {
-          id: db_event[0].user,
-          avatar: db_event[0].avatar ? db_event[0].avatar : '/img/users/anon.jpg',
-          display_name: db_event[0].display_name,
-          bio: db_event[0].bio,
-          rating: db_event[0].rating,
-          xp: db_event[0].xp ? db_event[0].xp : 'new'
-        },
-        tags: [],
-        students: [],
-        teachers: []
-      }
-      event.teachers.push(event.host);
-      query.get_users_by_event(id)
-        .then(db_users => {
-          db_users.forEach(user => {
-            user.role === 'student' ? event.students.push(user) : event.teachers.push(user);
-          })
-          event.vacancies = event.capacity - event.students.length;
-          query.get_tags_by_event(id)
-            .then(db_tags => {
-              db_tags.forEach(tag => {
-                event.tags.push(tag.title);
-              })
-              res.render('event', {
-                event
-              })
-            })
-            .catch(err => {
-              console.error(err);
-            })
+    let id = req.params.id;
+    query.get_event_by_id(id)
+        .then(db_event => {
+            let event = {
+                title: db_event[0].title,
+                description: db_event[0].description,
+                skill_level: db_event[0].skill_level,
+                capacity: db_event[0].capacity,
+                location: {
+                    title: db_event[0].venue,
+                    address: db_event[0].address,
+                    area: db_event[0].area
+                },
+                date: db_event[0].start.toLocaleDateString('en-us', {
+                    weekday: 'long',
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric'
+                }),
+                start: db_event[0].start.toLocaleTimeString('en-us', {
+                    hour: 'numeric',
+                    minute: '2-digit'
+                }),
+                end: db_event[0].end.toLocaleTimeString('en-us', {
+                    hour: 'numeric',
+                    minute: '2-digit'
+                }),
+                host: {
+                    id: db_event[0].user,
+                    avatar: db_event[0].avatar ? db_event[0].avatar : '/img/users/anon.jpg',
+                    display_name: db_event[0].display_name,
+                    bio: db_event[0].bio,
+                    rating: db_event[0].rating,
+                    xp: db_event[0].xp ? db_event[0].xp : 'new'
+                },
+                tags: [],
+                students: [],
+                teachers: []
+            }
+            event.teachers.push(event.host);
+            query.get_users_by_event(id)
+                .then(db_users => {
+                    db_users.forEach(user => {
+                        user.role === 'student' ? event.students.push(user) : event.teachers.push(user);
+                    })
+                    event.vacancies = event.capacity - event.students.length;
+                    query.get_tags_by_event(id)
+                        .then(db_tags => {
+                            db_tags.forEach(tag => {
+                                event.tags.push(tag.title);
+                            })
+                            res.render('event', {
+                                event
+                            })
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.render('error', {
+                                err
+                            });
+                        })
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.render('error', {
+                        err
+                    });
+                })
         })
         .catch(err => {
-          console.error(err);
+            console.error(err);
+            res.render('error', {
+                err
+            });
         })
-    })
-    .catch(err => {
-      console.error(err);
-    })
 })
 
 //UPDATE
 router.get('/update/:id', (req, res) => {
-  let id = Number(req.params.id);
-  query.get_event_by_id(id)
-    .then(event => {
-      query.get_tags_by_event(event)
-        .then(tags => {
-          query.get_users_by_event(event)
-            .then(users => {
-              res.render('update_user', {
-                event,
-                tags,
-                users
-              });
+    if (req.session.user) {
+        let id = Number(req.params.id);
+        query.get_event_by_id(id)
+            .then(event => {
+                if (event.user == req.session.user) {
+                    query.get_tags_by_event(event)
+                        .then(tags => {
+                            query.get_users_by_event(event)
+                                .then(users => {
+                                    res.render('update_user', {
+                                        event,
+                                        tags,
+                                        users
+                                    });
+                                })
+                                .catch(err => {
+                                    console.error(err);
+                                    res.render('error', {
+                                        err
+                                    });
+                                })
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.render('error', {
+                                err
+                            });
+                        })
+                }
             })
             .catch(err => {
-              console.error(err);
-              res.status(400).send(err);
+                console.error(err);
+                res.render('error', {
+                    err
+                });
             })
-        })
-        .catch(err => {
-          console.error(err);
-          res.status(400).send(err);
-        })
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(400).send(err);
-    })
+    } else {
+        res.redirect('/login?e=restricted');
+    }
 })
 
 router.put('/update', (req, res) => {
-  let id = Number(req.body.id);
-  query.update_event(id)
-    .then(id => {
-      res.redirect(`/event/update/${id}`);
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(400).send(err)
-    })
+    if (req.session.user) {
+        let id = Number(req.body.id);
+        query.get_event_by_id(id)
+            .then(event => {
+                if (req.session.user == event.user) {
+                    query.update_event(id)
+                        .then(id => {
+                            res.redirect(`/event/update/${id}`);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.render('error', {
+                                err
+                            })
+                        })
+                } else {
+                    res.redirect('/login?e=unauthorized');
+                }
+            })
+    } else {
+        res.redirect('/login?e=restricted')
+    }
 });
 
 //DELETE
-router.delete('/delete/:id', (req, res, next) => {
-  let id = Number(req.params.id);
-  query.delete_event(id)
-    .then(event => {
-      res.redirect('/events');
-    })
-    .catch(err => {
-      console.error(err);
-    })
+router.delete('/delete/:id', (req, res) => {
+    if (req.session.user) {
+        let id = Number(req.params.id);
+        query.get_event_by_id(id)
+            .then(event => {
+                if (event.user == req.session.user) {
+                    query.delete_event(id)
+                        .then(event => {
+                            res.redirect('/events');
+                        })
+                        .catch(err => {
+                            console.error(err);
+                        })
+                } else {
+                    res.redirect('/login?e=unauthorized');
+                }
+            })
+    } else {
+        res.redirect('/login?e=restricted');
+    }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,87 +1,91 @@
 const express = require('express'),
-  router = express.Router(),
-  query = require('../queries');
+    router = express.Router(),
+    query = require('../queries');
 
 router.get('/', (req, res, next) => {
-  query.get_events()
-    .then(values => {
-      let events = [];
-      query.get_all_tags()
-        .then(all_tag_titles => {
-          let all_tags = [];
-          all_tag_titles.forEach(a_tags => {
-            let all_tag = {
-              title: a_tags.title
-            }
-            all_tags.push(all_tag);
-          });
-          query.get_tags()
-            .then(tag_values => {
-              let tags = [];
-              tag_values.forEach(tag_value => {
-                let tag = {
-                  title: tag_value.title,
-                  event: tag_value.event
-                }
-                tags.push(tag);
-              });
-              values.forEach(value => {
-                let event = {
-                  id: value.id,
-                  title: value.title,
-                  location: {
-                    title: value.venue,
-                    address: value.address,
-                    area: value.area
-                  },
-                  date: value.start.toLocaleDateString('en-us', {
-                    month: 'numeric',
-                    day: '2-digit'
-                  }),
-                  start: value.start.toLocaleTimeString('en-us', {
-                    hour: 'numeric',
-                    minute: '2-digit'
-                  }),
-                  end: value.end.toLocaleTimeString('en-us', {
-                    hour: 'numeric',
-                    minute: '2-digit'
-                  }),
-                  tags: [],
-                  host: {
-                    id: value.user,
-                    avatar: value.avatar ? value.avatar : '/img/users/anon.jpg',
-                    display_name: value.display_name,
-                    bio: value.bio,
-                    rating: value.rating,
-                    xp: value.xp ? value.xp : 'new'
-                  },
-                  description: value.description,
-                  skill_level: value.skill_level,
-                  capacity: value.capacity
-                }
-                let filtered_tags = tags.filter(tag => tag.event === event.id);
-                filtered_tags.forEach(tag => event.tags.push(tag.title));
-                events.push(event);
-              });
-              res.render('index', {
-                events,
-                all_tags
-              });
+    if (req.session.user) {
+        res.redirect('/events');
+    } else {
+        query.get_events()
+            .then(values => {
+                let events = [];
+                query.get_all_tags()
+                    .then(all_tag_titles => {
+                        let all_tags = [];
+                        all_tag_titles.forEach(a_tags => {
+                            let all_tag = {
+                                title: a_tags.title
+                            }
+                            all_tags.push(all_tag);
+                        });
+                        query.get_tags()
+                            .then(tag_values => {
+                                let tags = [];
+                                tag_values.forEach(tag_value => {
+                                    let tag = {
+                                        title: tag_value.title,
+                                        event: tag_value.event
+                                    }
+                                    tags.push(tag);
+                                });
+                                values.forEach(value => {
+                                    let event = {
+                                        id: value.id,
+                                        title: value.title,
+                                        location: {
+                                            title: value.venue,
+                                            address: value.address,
+                                            area: value.area
+                                        },
+                                        date: value.start.toLocaleDateString('en-us', {
+                                            month: 'numeric',
+                                            day: '2-digit'
+                                        }),
+                                        start: value.start.toLocaleTimeString('en-us', {
+                                            hour: 'numeric',
+                                            minute: '2-digit'
+                                        }),
+                                        end: value.end.toLocaleTimeString('en-us', {
+                                            hour: 'numeric',
+                                            minute: '2-digit'
+                                        }),
+                                        tags: [],
+                                        host: {
+                                            id: value.user,
+                                            avatar: value.avatar ? value.avatar : '/img/users/anon.jpg',
+                                            display_name: value.display_name,
+                                            bio: value.bio,
+                                            rating: value.rating,
+                                            xp: value.xp ? value.xp : 'new'
+                                        },
+                                        description: value.description,
+                                        skill_level: value.skill_level,
+                                        capacity: value.capacity
+                                    }
+                                    let filtered_tags = tags.filter(tag => tag.event === event.id);
+                                    filtered_tags.forEach(tag => event.tags.push(tag.title));
+                                    events.push(event);
+                                });
+                                res.render('index', {
+                                    events,
+                                    all_tags
+                                });
+                            })
+                            .catch(err => {
+                                console.error(err)
+                                res.status(500).send(err);
+                            })
+                    })
+                    .catch(err => {
+                        console.error(err)
+                        res.status(500).send(err);
+                    });
             })
             .catch(err => {
-              console.error(err)
-              res.status(500).send(err);
-            })
-        })
-        .catch(err => {
-          console.error(err)
-          res.status(500).send(err);
-        });
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(500).send(err);
-    });
+                console.error(err)
+                res.status(500).send(err);
+            });
+    }
 });
 
 module.exports = router;
